f1d1c7e864882700048057bbd22e098bba602a5e
Baranidharan G <77617148+baranidharan-g23@users.noreply.github.com>
2023-01-24 15:40:42 +0530

    Create ultrasonic_sensor_wifi_firebase.ino

---
+++ ultrasonic_sensor_wifi_firebase.ino
@@ -0,0 +1,78 @@
+/*
+    This sketch establishes a TCP connection to a "quote of the day" service.
+    It sends a "hello" message, and then prints received data.
+*/
+#include <FirebaseESP8266.h>
+#include <ESP8266WiFi.h>
+#define ssid "DANIEL"
+#define password "pdlt2047"
+#define HOST "https://module-ecc50-default-rtdb.asia-southeast1.firebasedatabase.app/"
+#define AUTHENTICATION "8QzJMtJewyCF2DsRx6v1DvK8awkXBhHA4zQMq1Ax"
+#define trigPin 12
+#define echoPin 14
+
+//define sound velocity in cm/uS
+#define SOUND_VELOCITY 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+float distanceCm;
+float distanceInch;
+FirebaseData fbdo;
+void setup() {
+  Serial.begin(9600); // Starts the serial communication
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Firebase.begin(HOST,AUTHENTICATION);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate the distance
+  distanceCm = duration * SOUND_VELOCITY/2;
+  
+  // Convert to inches
+  distanceInch = distanceCm * CM_TO_INCH;
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  Serial.print("Distance (inch): ");
+  Serial.println(distanceInch);
+  
+  delay(1000);
+ Firebase.setInt(fbdo,"\DistanceinCm",distanceCm);
+}
